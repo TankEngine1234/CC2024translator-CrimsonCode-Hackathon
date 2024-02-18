cddc6f6b5a9181948a2f90acbf2ccb1f18a4b9f2
jinkokaki <zhu1dao3@gmail.com>
2024-02-18 02:12:24 -0800

    why are there so many changes??

--- process.py
+++
@@ -1,8 +0,0 @@
-import visual
-
-def main(file):
-    if file.filename.find('video') != -1:
-        visual.process_video(file.filename)
-    else:
-        visual.process_image(file.filename)
-    return file
